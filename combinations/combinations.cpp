93ee055bb77fef6078433b8b7adb54836295c170
Keshav_Thakur <65155383+Keshav-Thakur8@users.noreply.github.com>
2021-06-27 19:30:18 +0530

    Time: 36 ms (40.32%), Space: 51.9 MB (25.12%) - LeetHub

---
+++ combinations/combinations.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<vector<int>> res;
+    void helper(int si, int n, int k, vector<int> contribution){
+        //Base Case
+        if(k==0){
+            res.push_back(contribution);
+            return;
+        }
+            
+        if(si > n)
+            return;
+        //pruning:removing unwanted part
+        if(k>(n-si+1))
+            return;
+        
+        //Recursive Step
+        //c1
+        helper(si+1, n, k, contribution);
+        
+        //c2: include first index
+        contribution.push_back(si);
+        helper(si+1, n, k-1, contribution);
+    }
+    vector<vector<int>> combine(int n, int k) {
+        helper(1, n, k, {});
+        return res;
+    }
+};
