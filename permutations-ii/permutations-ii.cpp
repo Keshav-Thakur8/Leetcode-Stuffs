7e3e2ec09dee706fe8354c715e08124c87c8dc34
Keshav_Thakur <65155383+Keshav-Thakur8@users.noreply.github.com>
2021-06-27 19:07:31 +0530

    Time: 352 ms (6.24%), Space: 94 MB (5.01%) - LeetHub

---
+++ permutations-ii/permutations-ii.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<vector<int>> res;
+    void helper(vector<int>& A, vector<bool>&B, vector<int> contribution){
+        //Base Case
+        if(contribution.size() == A.size()){
+            res.push_back(contribution);
+        }
+        
+        //Recursive Step
+        for(int i=0 ; i<A.size() ; i++){
+            if(B[i] == false){
+                continue;
+            }
+            contribution.push_back(A[i]);
+            B[i] = false;
+            helper(A, B, contribution);
+            //undo step
+            contribution.pop_back();
+            B[i] = true;
+        }
+    }
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        int n = nums.size();
+        vector<bool> b(n, 1);
+        vector<int> contribution;
+        helper(nums, b, contribution);
+        sort(res.begin(), res.end());
+        res.erase(unique(res.begin(), res.end()), res.end());
+        return res;
+    }
+};
