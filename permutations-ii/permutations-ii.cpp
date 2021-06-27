8d8c3b0228dd46ba1cf6e5742b7ecd163379aa8f
Keshav_Thakur <65155383+Keshav-Thakur8@users.noreply.github.com>
2021-06-27 20:16:03 +0530

    Update permutations-ii.cpp

--- permutations-ii/permutations-ii.cpp
+++ permutations-ii/permutations-ii.cpp
@@ -1,32 +1,54 @@
 class Solution {
 public:
-    vector<vector<int>> res;
-    void helper(vector<int>& A, vector<bool>&B, vector<int> contribution){
-        //Base Case
-        if(contribution.size() == A.size()){
-            res.push_back(contribution);
-        }
+//     vector<vector<int>> res;
+//     void helper(vector<int>& A, vector<bool>&B, vector<int> contribution){
+//         //Base Case
+//         if(contribution.size() == A.size()){
+//             res.push_back(contribution);
+//         }
         
-        //Recursive Step
-        for(int i=0 ; i<A.size() ; i++){
-            if(B[i] == false){
+//         //Recursive Step
+//         for(int i=0 ; i<A.size() ; i++){
+//             if(B[i] == false){
+//                 continue;
+//             }
+//             contribution.push_back(A[i]);
+//             B[i] = false;
+//             helper(A, B, contribution);
+//             //undo step
+//             contribution.pop_back();
+//             B[i] = true;
+//         }
+//     }
+//     vector<vector<int>> permuteUnique(vector<int>& nums) {
+//         int n = nums.size();
+//         vector<bool> b(n, 1);
+//         vector<int> contribution;
+//         helper(nums, b, contribution);
+//         sort(res.begin(), res.end());
+//         res.erase(unique(res.begin(), res.end()), res.end());
+//         return res;
+//     }
+    
+    //Effective Solution
+    void helper(vector<int> v,vector<vector<int>> &ans,int idx){
+        if(idx == v.size()){
+            ans.push_back(v);
+            return;
+        }
+        for (int i = idx; i < v.size(); i++){
+            if(i != idx && v[i]==v[idx]){
                 continue;
             }
-            contribution.push_back(A[i]);
-            B[i] = false;
-            helper(A, B, contribution);
-            //undo step
-            contribution.pop_back();
-            B[i] = true;
+            swap(v[i],v[idx]);
+            helper(v,ans ,idx+1);
         }
+
     }
     vector<vector<int>> permuteUnique(vector<int>& nums) {
-        int n = nums.size();
-        vector<bool> b(n, 1);
-        vector<int> contribution;
-        helper(nums, b, contribution);
-        sort(res.begin(), res.end());
-        res.erase(unique(res.begin(), res.end()), res.end());
-        return res;
+        sort(nums.begin(),nums.end());
+        vector<vector<int>> ans;
+        helper(nums,ans,0);
+        return ans;
     }
 };
