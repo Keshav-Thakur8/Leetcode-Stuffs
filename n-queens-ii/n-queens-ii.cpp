93258848ef5f942055211fa7fd6f7ee97f8f3a52
Keshav_Thakur <65155383+Keshav-Thakur8@users.noreply.github.com>
2021-06-29 23:25:01 +0530

    Time: 4 ms (79.36%), Space: 6.2 MB (39.77%) - LeetHub

---
+++ n-queens-ii/n-queens-ii.cpp
@@ -0,0 +1,66 @@
+class Solution {
+public:
+    int ans = 0;
+    //function to check is it safe to put queen in a particular place 
+    bool isSafe(int row, int col, vector<vector<int>> &board, int n) {
+        //check upper diagonal
+        int i = row;
+        int j = col;
+        while(row >= 0 && col >= 0){
+            if(board[row][col] == 'Q') {
+                return false;
+            }
+            row--;
+            col--;
+        }
+        
+        //check left side
+        col = j;
+        row = i;
+        while(col >= 0) {
+            if(board[row][col] == 'Q') {
+                return false;
+            }
+            col--;
+            
+        }
+        
+        // check lower diagonal
+        col = j;
+        row = i;
+        while(row < n && col >= 0){
+            if(board[row][col] == 'Q') {
+                return false;
+            }
+            row++;
+            col--;
+        }
+        return true;
+        
+    }
+    
+    //Backtracking and recursion function
+    void solver(int col, vector<vector<int>> &board, int n) {
+        //Base Case
+        if(col == n){
+            ans++;
+            return;
+        }
+        
+        //Recursive Step
+        for(int row = 0 ; row < n ; row++) {
+            if(isSafe(row, col, board, n)) {
+                board[row][col] = 'Q';
+                solver(col+1, board, n);
+                //Backtrack
+                board[row][col] = '.';
+            }
+        }
+    }
+    int totalNQueens(int n) {
+        vector<vector<int>> board( n , vector<int> (n, 0));
+        solver(0,board,n);
+        return ans;
+        
+    }
+};
