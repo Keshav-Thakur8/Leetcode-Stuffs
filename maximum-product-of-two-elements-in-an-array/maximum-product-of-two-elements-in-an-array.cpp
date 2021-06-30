e66a5f87550ce8b26f1a325341e3f20ce0d31544
Keshav_Thakur <65155383+Keshav-Thakur8@users.noreply.github.com>
2021-06-30 23:24:04 +0530

    Update maximum-product-of-two-elements-in-an-array.cpp

--- maximum-product-of-two-elements-in-an-array/maximum-product-of-two-elements-in-an-array.cpp
+++ maximum-product-of-two-elements-in-an-array/maximum-product-of-two-elements-in-an-array.cpp
@@ -2,8 +2,14 @@ class Solution {
 public:
     int maxProduct(vector<int>& nums) {
         int n = nums.size();
+        for(int i=0 ; i<n ; i++){
+            nums[i]--;
+        }
         sort(nums.begin(), nums.end(), greater<int>());
-        return (nums[0]-1)*(nums[1]-1);
+        return nums[0]*nums[1];
+        //Two line solution
+        // sort(nums.begin(), nums.end(), greater<int>());
+        // return (nums[0]-1)*(nums[1]-1);
         
     }
 };
