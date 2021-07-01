374256531f40d6fa309d62392cf42bef6032924d
Keshav_Thakur <65155383+Keshav-Thakur8@users.noreply.github.com>
2021-07-01 23:02:35 +0530

    Update h-index.cpp

--- h-index/h-index.cpp
+++ h-index/h-index.cpp
@@ -5,20 +5,17 @@ public:
         int n =  citations.size();
         int i = 0;
         int j = n-1;
-        int mid;
+        int mid, ans=0;
         while(i <= j)
         {
             mid = i + (j-i) / 2;
-            
-            if(citations[mid] == (n - mid))
-                return citations[mid];
-            
-            else if(citations[mid] > (n - mid))
-                 j = mid - 1;
-            
+            if(citations[mid] >= (n - mid)){
+                ans = n-mid;
+                j = mid - 1;
+            }
             else
                 i = mid + 1;
         }
-        return n - i;
+        return ans;
     }
 };
