7ff5c301c16dd9245c5e8e4af43c96ff55e2f74e
Keshav_Thakur <65155383+Keshav-Thakur8@users.noreply.github.com>
2021-07-01 20:52:42 +0530

    Update majority-element.cpp

--- majority-element/majority-element.cpp
+++ majority-element/majority-element.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
         unordered_map<int,int> m;
-        for(int i=0 ; i<nums.size() ; i++){
-            m[nums[i]]++;
-        }
+        // for(int i=0 ; i<nums.size() ; i++){
+        //     m[nums[i]]++;
+        // }
         int occurence = nums.size()/2;
         for(int i=0 ; i<nums.size() ; i++){
-            if(m[nums[i]] > occurence){
+            if(++m[nums[i]] > occurence){
                 return nums[i];
             }
         }
