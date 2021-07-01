171267259263022303143543b396879edd2c529b
Keshav_Thakur <65155383+Keshav-Thakur8@users.noreply.github.com>
2021-07-01 19:57:43 +0530

    Update squares-of-a-sorted-array.cpp
    Optimized solution

--- squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
+++ squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
@@ -2,11 +2,26 @@ class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
         int n = nums.size();
-        for(int i=0 ; i<n ; i++){
-            nums[i] *= nums[i];
+        // for(int i=0 ; i<n ; i++){
+        //     nums[i] *= nums[i];
+        // }
+        // sort(nums.begin(), nums.end());
+        // return nums;
+        //Optimized solution
+        int si = 0;
+        int ei = n-1;
+        vector<int> res(n);
+        for(int k=ei ; k>=0 ; k--){
+            if(abs(nums[si]) > abs(nums[ei])){
+                res[k] = nums[si]*nums[si];
+                si++;
+            }
+            else {
+                res[k] = nums[ei]*nums[ei];
+                ei--;
+            }
         }
-        sort(nums.begin(), nums.end());
-        return nums;
+        return res;
         
     }
 };
