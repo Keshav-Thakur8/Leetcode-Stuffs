e77b082bba12aeb3f9eeb60c5ad0b0b70fc93326
Keshav_Thakur <65155383+Keshav-Thakur8@users.noreply.github.com>
2021-07-01 20:48:16 +0530

    Time: 24 ms (27.50%), Space: 19.7 MB (8.54%) - LeetHub

---
+++ majority-element/majority-element.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        unordered_map<int,int> m;
+        for(int i=0 ; i<nums.size() ; i++){
+            m[nums[i]]++;
+        }
+        int occurence = nums.size()/2;
+        for(int i=0 ; i<nums.size() ; i++){
+            if(m[nums[i]] > occurence){
+                return nums[i];
+            }
+        }
+        return 0;
+        
+        
+    }
+};
