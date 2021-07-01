0e0803678151b155a81eb8e0861774adb49c996a
Keshav_Thakur <65155383+Keshav-Thakur8@users.noreply.github.com>
2021-07-01 22:54:53 +0530

    Time: 16 ms (73.97%), Space: 18.7 MB (44.38%) - LeetHub

---
+++ h-index-ii/h-index-ii.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+        int n =  citations.size();
+        int i = 0;
+        int j = n-1;
+        int mid;
+        while(i <= j)
+        {
+            mid = i + (j-i) / 2;
+            
+            if(citations[mid] == (n - mid))
+                return citations[mid];
+            
+            else if(citations[mid] > (n - mid))
+                 j = mid - 1;
+            
+            else
+                i = mid + 1;
+        }
+        return n - i;
+    }
+};
