b53a57f28516b70c38035b9b762cc01cfdee9977
Keshav_Thakur <65155383+Keshav-Thakur8@users.noreply.github.com>
2021-07-02 23:33:02 +0530

    Update find-all-duplicates-in-an-array.cpp

--- find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.cpp
+++ find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.cpp
@@ -2,16 +2,24 @@ class Solution {
 public:
     vector<int> findDuplicates(vector<int>& nums) {
         int n = nums.size();
-        unordered_map<int,int> m;
-        for(int i=0 ; i<n ; i++){
-            m[nums[i]]++;
-        }
         vector<int> ans;
-        for(int i=1 ; i<=n ; i++){
-            if(m[i] == 2){
-                ans.push_back(i);
+        for(int i=0 ; i<n ; i++){
+            if(nums[abs(nums[i])-1] < 0){
+                ans.push_back(abs(nums[i]));
+            } else{
+                nums[abs(nums[i])-1] *= -1;
             }
         }
+        // unordered_map<int,int> m;
+        // for(int i=0 ; i<n ; i++){
+        //     m[nums[i]]++;
+        // }
+        // vector<int> ans;
+        // for(int i=1 ; i<=n ; i++){
+        //     if(m[i] == 2){
+        //         ans.push_back(i);
+        //     }
+        // }
         return ans;
     }
 };
