daa817261af098eb1e0dc3c60ad2d9db22e2fee5
Keshav_Thakur <65155383+Keshav-Thakur8@users.noreply.github.com>
2021-07-02 23:18:04 +0530

    Update find-all-numbers-disappeared-in-an-array.cpp

--- find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.cpp
+++ find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.cpp
@@ -1,16 +1,29 @@
 class Solution {
 public:
     vector<int> findDisappearedNumbers(vector<int>& nums) {
-        vector<int> ans;
-        unordered_map<int,int> m;
-        for(int i=0 ; i<nums.size() ; i++){
-            m[nums[i]]++;
+        //optimized solution
+        int n = nums.size();
+        for(int i=0 ; i<n ; i++){
+            int x = abs(nums[i])-1;
+            nums[x] = nums[x]>0 ? -nums[x] : nums[x];
         }
-        for(int i=1 ; i<=nums.size() ; i++){
-            if(m[i] == 0){
-                ans.push_back(i);
+        
+        vector<int> ans;
+        for(int i=0 ; i<n ; i++){
+            if(nums[i]>0){
+                ans.push_back(i+1);
             }
         }
+        
+        // unordered_map<int,int> m;
+        // for(int i=0 ; i<nums.size() ; i++){
+        //     m[nums[i]]++;
+        // }
+        // for(int i=1 ; i<=nums.size() ; i++){
+        //     if(m[i] == 0){
+        //         ans.push_back(i);
+        //     }
+        // }
         return ans;
     }
 };
