569f31bcd636db8564c01c961119080506de2342
Keshav_Thakur <65155383+Keshav-Thakur8@users.noreply.github.com>
2021-07-03 19:47:19 +0530

    Update merge-sorted-array.cpp

--- merge-sorted-array/merge-sorted-array.cpp
+++ merge-sorted-array/merge-sorted-array.cpp
@@ -1,14 +1,37 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int k = 0;
-        int j = m;
-        while(j<=m+n && k<n){
-            nums1[j] = nums2[k];
-            j++;
-            k++;
+        //Simple approach
+        // int k = 0;
+        // int j = m;
+        // while(j<=m+n && k<n){
+        //     nums1[j] = nums2[k];
+        //     j++;
+        //     k++;
+        // }
+        // sort(nums1.begin(),nums1.end());
+        //start traversing array from the end
+        int i = m-1;
+        int j = n-1;
+        int k = m+n-1;
+        while(i>=0 && j>=0){
+            if(nums1[i] > nums2[j]){
+                nums1[k] = nums1[i];
+                k--;
+                i--;
+            } else {
+                nums1[k] = nums2[j];
+                k--;
+                j--;
+            }
         }
-        sort(nums1.begin(),nums1.end());
+        //this loop is for the case in which nums2 is longer than nums1
+        //simply copy rest of the element to nums1
+        while(j>=0){
+            nums1[k] = nums2[j];
+            k--;
+            j--;
+        }
         
         
     }
