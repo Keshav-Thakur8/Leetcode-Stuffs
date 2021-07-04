119fb353144163fbcaf7d7d6e56c582e4cd245a4
Keshav_Thakur <65155383+Keshav-Thakur8@users.noreply.github.com>
2021-07-04 23:35:25 +0530

    Update valid-anagram.cpp

--- valid-anagram/valid-anagram.cpp
+++ valid-anagram/valid-anagram.cpp
@@ -1,14 +1,26 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        if(s == t){
-            return true;
+        vector<int> freq(26,0);
+        for(int i=0 ; i<s.size() ; i++){
+            freq[s[i]-'a']++;
         }
-        else{
-            return false;
+        for(int j=0 ; j<t.size() ; j++){
+            freq[t[j]-'a']--;
         }
+        for(int k=0 ; k<freq.size() ; k++){
+            if(freq[k] != 0)
+                return false;
+        }
+        return true;
+        // sort(s.begin(),s.end());
+        // sort(t.begin(),t.end());
+        // if(s == t){
+        //     return true;
+        // }
+        // else{
+        //     return false;
+        // }
         
     }
 };
