2343894e6497be279d2856e4ee440014e9330394
Keshav_Thakur <65155383+Keshav-Thakur8@users.noreply.github.com>
2021-07-04 20:36:44 +0530

    Update reverse-pairs.cpp

--- reverse-pairs/reverse-pairs.cpp
+++ reverse-pairs/reverse-pairs.cpp
@@ -2,13 +2,16 @@ class Solution {
 public:
     int merge(vector<int>& nums, int start,int mid,int end){
         int cnt = 0;
-        int j = mid+1;
+        int j = mid+1; //starting point of right half array
+        //main part
+        //for every left element we will check right element
         for(int i=start ; i<=mid ; i++){
             while(j<=end && nums[i]>(long)2*nums[j]){
                 j++;
             }
             cnt += j-(mid+1);
         }
+        //merge process from here
         vector<int> helper;
         int left = start;
         int right = mid+1;
@@ -21,6 +24,7 @@ public:
                 right++;
             }
         }
+        //if left or right get exhausted
         while(left<=mid){
             helper.push_back(nums[left]);
             left++;
@@ -36,11 +40,14 @@ public:
         return cnt;
     }
     int merge_sort(vector<int>& nums,int start,int end){
+        //Base Case:Single element in the vector
         if(start>=end){
             return 0;
         }
         int mid = (start+end)/2;
+        //Left part Recursion
         int inv = merge_sort(nums,start,mid);
+        //Right part Recursion
         inv += merge_sort(nums,mid+1,end);
         inv += merge(nums,start,mid,end);
         return inv;
