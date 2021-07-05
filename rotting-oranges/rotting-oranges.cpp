67cc43d58108effd19fa23b75914a615881c82a6
Keshav_Thakur <65155383+Keshav-Thakur8@users.noreply.github.com>
2021-07-06 00:26:25 +0530

    Time: 8 ms (63.25%), Space: 13.8 MB (16.35%) - LeetHub

---
+++ rotting-oranges/rotting-oranges.cpp
@@ -0,0 +1,53 @@
+class Solution {
+public:
+    int orangesRotting(vector<vector<int>>& grid) {
+        //number of fresh orange
+        int fresh = 0;
+        int time = -1;
+        //queue to store pairs of coordinates
+        queue<vector<int>> q;
+        //4 direction move
+        vector<vector<int>> dir = {{1,0},{0,1},{-1,0},{0,-1}};
+        for(int i=0 ; i<grid.size() ; i++){
+            for(int j=0 ; j<grid[0].size() ; j++){
+                //count of valid oranges
+                if(grid[i][j] > 0)
+                    fresh++;
+                //only rotten oranges must be on initial step of queue
+                if(grid[i][j] == 2)
+                    q.push({i,j});
+            }
+        }
+        
+        //breadth first search
+        while(!q.empty()){
+            //one step from rotten
+            time++;
+            int queuesize = q.size();
+            for(int k=0 ; k<queuesize ; k++){
+                //take node from front of queue
+                vector<int> curr = q.front();
+                //decrese fresh orange count
+                fresh--;
+                q.pop();
+                //look in all four direction
+                for(int i=0 ; i<4 ; i++){
+                    //record the coordinates
+                    int x_cor = curr[0]+dir[i][0];
+                    int y_cor = curr[1]+dir[i][1];
+                    //if we go out of order or find orange which is not fresh
+                    if(x_cor>=grid.size() || x_cor<0 || y_cor>=grid[0].size() || y_cor<0 || grid[x_cor][y_cor]!=1)
+                        continue;
+                    //orange kharab ho gaya
+                    grid[x_cor][y_cor] = 2;
+                    //coordinates of rotten orange
+                    q.push({x_cor,y_cor});
+                }
+            }
+        }
+        //if we looked through all oranges, return result, else -1
+        //here max(0,time) is for case [[0]] as input
+        if(fresh==0) return max(0,time);
+        return -1;
+    }
+};
