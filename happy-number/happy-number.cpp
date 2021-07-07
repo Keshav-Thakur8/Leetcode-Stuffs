e9df2f1a8c664c76608c36fa181f0b0689c5ad06
Keshav_Thakur <65155383+Keshav-Thakur8@users.noreply.github.com>
2021-07-07 20:56:53 +0530

    Update happy-number.cpp

--- happy-number/happy-number.cpp
+++ happy-number/happy-number.cpp
@@ -9,13 +9,24 @@ public:
         }
         return sum;
     }
-        bool isHappy(int n) {
+        // bool isHappy(int n) {
+        //     int slow = n;
+        //     int fast = digitSquareSum(n);
+        //     while(slow != fast){
+        //         slow = digitSquareSum(slow);
+        //         fast = digitSquareSum(digitSquareSum(fast));
+        //     }
+        //     if(slow == 1)
+        //         return true;
+        //     else
+        //         return false;
+    bool isHappy(int n) {
             int slow = n;
-            int fast = digitSquareSum(n);
-            while(slow != fast){
+            int fast = n;
+            do{
                 slow = digitSquareSum(slow);
                 fast = digitSquareSum(digitSquareSum(fast));
-            }
+            }while(slow != fast);
             if(slow == 1)
                 return true;
             else
