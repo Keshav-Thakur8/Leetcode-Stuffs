3eeecb838a6a24e690edf8946ce5b130c245cdcb
Keshav_Thakur <65155383+Keshav-Thakur8@users.noreply.github.com>
2021-07-07 20:53:06 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (94.28%) - LeetHub

---
+++ happy-number/happy-number.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+        int digitSquareSum(int n) {
+        int sum = 0, tmp;
+        while (n) {
+            tmp = n % 10;
+            sum += tmp * tmp;
+            n /= 10;
+        }
+        return sum;
+    }
+        bool isHappy(int n) {
+            int slow = n;
+            int fast = digitSquareSum(n);
+            while(slow != fast){
+                slow = digitSquareSum(slow);
+                fast = digitSquareSum(digitSquareSum(fast));
+            }
+            if(slow == 1)
+                return true;
+            else
+                return false;
+        }
+};
