a7773ec8e99a54a03ec4138f212dba4ad05d5e0c
Keshav_Thakur <65155383+Keshav-Thakur8@users.noreply.github.com>
2021-07-14 00:03:34 +0530

    Time: 28 ms (82.30%), Space: 13.2 MB (35.92%) - LeetHub

---
+++ maximal-rectangle/maximal-rectangle.cpp
@@ -0,0 +1,73 @@
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& heights) {
+        int n = heights.size();
+        //Next smaller elements
+        vector<int> next_small(n,-1);
+        stack<int> s;
+        for(int i=0 ; i<n ; i++){
+            while(!s.empty() && heights[s.top()] > heights[i]){
+                next_small[s.top()] = i;
+                s.pop();
+            }
+            s.push(i);
+        }
+        while(!s.empty()){
+            next_small[s.top()] = n;
+            s.pop();
+        }
+        
+        //Previous Smaller Elements
+        //we can use same stack since here the stack will be empty
+        vector<int> prev_small(n,-1);
+        for(int i=n-1 ; i>=0 ; i--){
+            while(!s.empty() && heights[s.top()] > heights[i]){
+                prev_small[s.top()] = i;
+                s.pop();
+            }
+            s.push(i);
+        }
+        while(!s.empty()){
+            prev_small[s.top()] = -1;
+            s.pop();
+        }
+        
+        //For every i find the array
+        int max_height = INT_MIN;
+        for(int i=0 ; i<n ; i++){
+            //suppose 2nd index is the prex small
+            //ans 5th index is the next small
+            //actual rectangle lies from the 3rd index to the 4th index
+            //so width is 2
+            int width = next_small[i]-prev_small[i]-1;
+            int area = width*heights[i];
+            max_height = max(max_height,area);
+        }
+        return max_height;
+        
+    }
+    int maximalRectangle(vector<vector<char>>& matrix) {
+        int m=matrix.size();
+        if(m==0) {
+            return 0;
+        }
+        int n=matrix[0].size();
+        int res=0;
+        vector<int> histogram(n, 0);
+        
+        //find histogram array for each row
+        //keep in mind that we are not updating our vector histogram
+        //which is helping the most
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(matrix[i][j]=='1')
+                    histogram[j]+=1;
+                else
+                    histogram[j]=0;
+            }
+            
+            res = max(res, largestRectangleArea(histogram));
+        }
+        return res;
+    }
+};
