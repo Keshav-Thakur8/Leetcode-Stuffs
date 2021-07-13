e19bfacf9f26edbef1196e118ab0d26cdd7cf03b
Keshav_Thakur <65155383+Keshav-Thakur8@users.noreply.github.com>
2021-07-13 19:38:25 +0530

    Time: 148 ms (45.42%), Space: 88.8 MB (31.93%) - LeetHub

---
+++ daily-temperatures/daily-temperatures.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        int n = temperatures.size();
+        stack<int> s;
+        vector<int> ans(n,-1);
+        for(int i=0 ; i<n ; i++){
+            // Finding out all days for which A[i] is the answer.
+            // Finding out days < i s.t. A[j] < A[i] and i is the first such index.
+            // Stack all the unprocessed days.
+            // Indices in the stack.
+            while(!s.empty() && temperatures[i]>temperatures[s.top()]){
+                ans[s.top()] = i-s.top();
+                s.pop();
+            }
+            s.push(i);
+        }
+        while(!s.empty()){
+            ans[s.top()] = 0;
+            s.pop();
+        }
+        return ans;
+    }
+};
