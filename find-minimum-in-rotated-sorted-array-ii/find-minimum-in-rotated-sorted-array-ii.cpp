e60db2b7e805a5d6c872b734e2167b134c8a2daf
Keshav_Thakur <65155383+Keshav-Thakur8@users.noreply.github.com>
2021-07-16 00:50:49 +0530

    Update find-minimum-in-rotated-sorted-array-ii.cpp

--- find-minimum-in-rotated-sorted-array-ii/find-minimum-in-rotated-sorted-array-ii.cpp
+++ find-minimum-in-rotated-sorted-array-ii/find-minimum-in-rotated-sorted-array-ii.cpp
@@ -7,21 +7,14 @@ public:
         int mid;
         while(si < ei){
             mid = si + (ei-si)/2;
-            //for removing duplicates
-            if(nums[mid] == nums[ei]){
-                ei--;
-                continue;
-            }
-            if(nums[si] < nums[ei]){
-                return nums[si];
-            }
-            //this condition will handel the case in which vector contains only two integers
-            //and the second one is smaller ten the first element
-            if(nums[mid] >= nums[si]){
+            if(nums[mid] > nums[ei]){
                 si = mid+1;
             }
-            else{
+            else if(nums[mid] < nums[ei]){
                 ei = mid;
+            }
+            else{
+                ei--;
             }
         }
         //when we have only element in the array
