fae97a14955f09122c75793c385e2ccb417a70e5
Keshav_Thakur <65155383+Keshav-Thakur8@users.noreply.github.com>
2021-07-16 20:23:28 +0530

    Update longest-consecutive-sequence.cpp

--- longest-consecutive-sequence/longest-consecutive-sequence.cpp
+++ longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -1,24 +1,47 @@
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-        //Brute Force
-        sort(nums.begin(),nums.end());
-        int cnt = 0, ans = 0;
-        if(nums.size() == 0){
-            return 0;
+        //optimal solution
+        set<int> hash;
+        //insert elements of nums into the set
+        for(int i=0 ; i<nums.size() ; i++){
+            hash.insert(nums[i]);
         }
-        for(int i=0 ; i<nums.size()-1 ; i++){
-            if(nums[i] == nums[i+1]-1){
-                cnt++;
-                ans = max(cnt,ans);
-            }
-            else if(nums[i] == nums[i+1]){
-                continue;
-            }
-            else{
-                cnt = 0;
+        int ans = 0;
+        for(int i=0 ; i<nums.size() ; i++){
+            //for every element in the vector check if one lesser then it exist or not
+            //if it exists do nothing
+            //otherwise start moving one step ahead of that number till you encounter final value
+            //then check if it is maximum or not
+            if(!hash.count(nums[i]-1)){
+                int curr_num = nums[i];
+                int curr_cnt = 1;
+                while(hash.count(curr_num+1)){
+                    curr_num += 1;
+                    curr_cnt++;
+                }
+                ans = max(ans,curr_cnt);
             }
         }
-        return ans+1;
+        return ans;
+        // //Brute Force
+        // sort(nums.begin(),nums.end());
+        // int cnt = 0, ans = 0;
+        // if(nums.size() == 0){
+        //     return 0;
+        // }
+        // for(int i=0 ; i<nums.size()-1 ; i++){
+        //     if(nums[i] == nums[i+1]-1){
+        //         cnt++;
+        //         ans = max(cnt,ans);
+        //     }
+        //     else if(nums[i] == nums[i+1]){
+        //         continue;
+        //     }
+        //     else{
+        //         cnt = 0;
+        //     }
+        // }
+        // return ans+1;
     }
 };
