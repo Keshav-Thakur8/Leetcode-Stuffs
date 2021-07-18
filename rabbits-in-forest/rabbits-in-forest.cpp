bca6c48dad34a71b0765bb8821807777d539bff6
Keshav_Thakur <65155383+Keshav-Thakur8@users.noreply.github.com>
2021-07-19 03:30:38 +0530

    Update rabbits-in-forest.cpp

--- rabbits-in-forest/rabbits-in-forest.cpp
+++ rabbits-in-forest/rabbits-in-forest.cpp
@@ -1,13 +1,18 @@
 class Solution {
 public:
+    //notes dekho
     int numRabbits(vector<int>& answers) {
         unordered_map<int,int> mp;
-        for(int i : answers) {
-            mp[i]++;
+        for(int i=0 ; i<answers.size() ; i++) {
+            mp[answers[i]]++;
         }
         int ans = 0;
         for(auto i : mp) {
-            ans += (i.first+i.second)/(i.first+1) * (i.first+1);
+            // ans += (i.first+i.second)/(i.first+1) * (i.first+1);
+            int gs = i.first+1; // group size
+            int reportees = i.second; // rabbit who report there types
+            int ng = (i.second+i.first)/(i.first+1); //number of minimum groups required
+            ans += ng*gs;
         }
         return ans;
     }
