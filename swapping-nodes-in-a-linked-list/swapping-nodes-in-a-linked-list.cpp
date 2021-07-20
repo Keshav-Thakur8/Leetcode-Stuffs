1120b4c2f095990d7ebe48cbe49d2834b74f27a8
Keshav_Thakur <65155383+Keshav-Thakur8@users.noreply.github.com>
2021-07-21 03:00:32 +0530

    Update swapping-nodes-in-a-linked-list.cpp

--- swapping-nodes-in-a-linked-list/swapping-nodes-in-a-linked-list.cpp
+++ swapping-nodes-in-a-linked-list/swapping-nodes-in-a-linked-list.cpp
@@ -11,21 +11,38 @@
 class Solution {
 public:
     ListNode* swapNodes(ListNode* head, int k) {
-        ListNode* temp = head;
-        int cnt = 0;
-        while(temp != NULL){
-            cnt++;
-            temp = temp->next;
-        }
-        temp = head;
+        //We traverse the list until we reach the kth node.
+        //Then we set kth to that node to remember it, set ptr2 to head and continue traversing the list, while we               //move both ptr1 and ptr2.
+        //When ptr1 reaches the end - ptr2 is on the (n-k)th node.
         ListNode* temp1 = head;
-        for(int i=1 ; i<k ; i++) {
-            temp = temp->next;
-        }
-        for(int i=1 ; i<cnt-k+1 ; i++) {
+        ListNode* temp2 = head;
+        ListNode* kth = NULL;
+        while(--k != 0) {
             temp1 = temp1->next;
         }
-        swap(temp->val, temp1->val);
+        kth = temp1;
+        temp1 = temp1->next;
+        while(temp1 != NULL) {
+            temp1 = temp1->next;
+            temp2 = temp2->next;
+        }
+        swap(temp2->val, kth->val);
         return head;
+        // ListNode* temp = head;
+        // int cnt = 0;
+        // while(temp != NULL){
+        //     cnt++;
+        //     temp = temp->next;
+        // }
+        // temp = head;
+        // ListNode* temp1 = head;
+        // for(int i=1 ; i<k ; i++) {
+        //     temp = temp->next;
+        // }
+        // for(int i=1 ; i<cnt-k+1 ; i++) {
+        //     temp1 = temp1->next;
+        // }
+        // swap(temp->val, temp1->val);
+        // return head;
     }
 };
