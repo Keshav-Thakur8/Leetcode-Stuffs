6afe2b54cb6b9d623279b45de6d962c2af175176
Keshav_Thakur <65155383+Keshav-Thakur8@users.noreply.github.com>
2021-07-20 21:03:55 +0530

    Update longest-substring-without-repeating-characters.cpp

--- longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -1,25 +1,47 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
+        //hashmap approach
+        unordered_map<char,int> mp;
         int n = s.size();
         if(n == 0) {
-            return 0;
+             return 0;
         }
-        set<char> st;
         int left = 0;
         int right = 0;
-        int max_length = INT_MIN;
+        int len = INT_MIN;
         while(right < n) {
-            if(st.count(s[right]) == 0) {
-                st.insert(s[right]);
-                max_length = max(max_length, right-left+1);
-                right++;
+            if(mp.find(s[right]) != mp.end()) {
+                left = max(mp[s[right]]+1, left);
             }
-            else {
-                st.erase(s[left]);
-                left++;
-            }
+            mp[s[right]] = right;
+            
+            len = max(len,right-left+1);
+            right++;
         }
-        return max_length;
+        return len;
+        
+        // //sliding window approach
+        // //TC: O(2n)
+        // int n = s.size();
+        // if(n == 0) {
+        //     return 0;
+        // }
+        // set<char> st;
+        // int left = 0;
+        // int right = 0;
+        // int max_length = INT_MIN;
+        // while(right < n) {
+        //     if(st.count(s[right]) == 0) {
+        //         st.insert(s[right]);
+        //         max_length = max(max_length, right-left+1);
+        //         right++;
+        //     }
+        //     else {
+        //         st.erase(s[left]);
+        //         left++;
+        //     }
+        // }
+        // return max_length;
     }
 };
