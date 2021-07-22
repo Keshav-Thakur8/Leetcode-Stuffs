2ac78f1ab9546ec018c49c3d56d22c3cd5294d55
Keshav_Thakur <65155383+Keshav-Thakur8@users.noreply.github.com>
2021-07-22 15:55:45 +0530

    Update symmetric-tree.cpp

--- symmetric-tree/symmetric-tree.cpp
+++ symmetric-tree/symmetric-tree.cpp
@@ -12,17 +12,42 @@
 class Solution {
 public:
     bool helper(TreeNode* l, TreeNode* r) {
+        // Iterative Solution
+        queue<TreeNode*> que;
+        que.push(l);
+        que.push(r);
+        while(!que.empty()) {
+            TreeNode* l1 = que.front();
+            que.pop();
+            TreeNode* r1 = que.front();
+            que.pop();
+            if(l1 == NULL && r1 == NULL) {
+                continue;
+            }
+            if(l1 == NULL || r1 == NULL) {
+                return false;
+            }
+            if(l1->val != r1->val) {
+                return false;
+            }
+            que.push(l1->left);
+            que.push(r1->right);
+            que.push(l1->right);
+            que.push(r1->left);
+        }
+        return true;
+        
         // Recursive Solution
-        if(l == NULL && r == NULL) {
-            return true;
-        }
-        if(l == NULL || r == NULL) {
-            return false;
-        }
-        if(l->val != r->val) {
-            return false;
-        }
-        return (helper(l->left,r->right) && helper(l->right, r->left));
+        // if(l == NULL && r == NULL) {
+        //     return true;
+        // }
+        // if(l == NULL || r == NULL) {
+        //     return false;
+        // }
+        // if(l->val != r->val) {
+        //     return false;
+        // }
+        // return (helper(l->left,r->right) && helper(l->right, r->left));
         
     }
     bool isSymmetric(TreeNode* root) {
