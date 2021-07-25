76efdbb77527a66a563e91df5f0ad18952f1d2fa
Keshav_Thakur <65155383+Keshav-Thakur8@users.noreply.github.com>
2021-07-25 21:03:00 +0530

    Update n-ary-tree-postorder-traversal.cpp

--- n-ary-tree-postorder-traversal/n-ary-tree-postorder-traversal.cpp
+++ n-ary-tree-postorder-traversal/n-ary-tree-postorder-traversal.cpp
@@ -20,17 +20,34 @@ public:
 
 class Solution {
 public:
-    //recursive
+    //iterative
     void solve(Node* root, vector<int>& res) {
         if(root == NULL) {
             return;
         }
-        for(int i=0 ; i<root->children.size() ; i++) {
-            solve(root->children[i], res);
+        stack<Node*> st;
+        st.push(root);
+        while(!st.empty()) {
+            Node* fnt = st.top();
+            st.pop();
+            for(int i=0 ; i<fnt->children.size() ; i++) {
+                st.push(fnt->children[i]);
+            }
+            res.push_back(fnt->val);
         }
-        res.push_back(root->val);
-        
+        reverse(res.begin(), res.end());
     }
+//     //recursive
+//     void solve(Node* root, vector<int>& res) {
+//         if(root == NULL) {
+//             return;
+//         }
+//         for(int i=0 ; i<root->children.size() ; i++) {
+//             solve(root->children[i], res);
+//         }
+//         res.push_back(root->val);
+        
+//     }
     
     vector<int> postorder(Node* root) {
         vector<int> res;
