c2ca1a7fa05f5b135d47c50a86f0ba942eaf6b04
Keshav_Thakur <65155383+Keshav-Thakur8@users.noreply.github.com>
2021-08-01 19:47:55 +0530

    Update implement-queue-using-stacks.cpp

--- implement-queue-using-stacks/implement-queue-using-stacks.cpp
+++ implement-queue-using-stacks/implement-queue-using-stacks.cpp
@@ -50,3 +50,66 @@ public:
  * int param_3 = obj->peek();
  * bool param_4 = obj->empty();
  */
+
+
+
+
+//Follow-up: Can you implement the queue such that each operation is amortized O(1) time complexity? 
+//In other words, performing n operations will take overall O(n) time even if one of those operations may take longer.
+class MyQueue {
+public:
+    /** Initialize your data structure here. */
+    stack<int> input, output;
+    MyQueue() {
+        
+    }
+    
+    /** Push element x to the back of queue. */
+    void push(int x) {
+        input.push(x);
+    }
+    
+    /** Removes the element from in front of queue and returns that element. */
+    int pop() {
+        //if output is empty
+        if(output.empty()) {
+            while(input.size()) {
+                output.push(input.top());
+                input.pop();
+            }
+            
+        }
+        //if output is non-empty
+        int val = output.top();
+        output.pop();
+        return val;
+        
+    }
+    
+    /** Get the front element. */
+    int peek() {
+        //if output is empty
+        if(output.empty()) {
+            while(input.size()) {
+                output.push(input.top());
+                input.pop();
+            }
+        }
+        //if output is non-empty
+        return output.top();
+    }
+    
+    /** Returns whether the queue is empty. */
+    bool empty() {
+        return input.empty() && output.empty();
+    }
+};
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue* obj = new MyQueue();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->peek();
+ * bool param_4 = obj->empty();
+ */
