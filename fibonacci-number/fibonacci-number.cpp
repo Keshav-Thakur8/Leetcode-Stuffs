5cc2dc636c5090a096bf6bfb7276cb50bf930f53
Keshav_Thakur <65155383+Keshav-Thakur8@users.noreply.github.com>
2021-08-11 19:34:02 +0530

    Update fibonacci-number.cpp

--- fibonacci-number/fibonacci-number.cpp
+++ fibonacci-number/fibonacci-number.cpp
@@ -1,13 +1,30 @@
 class Solution {
 public:
-    int fib(int n) {
+    int solve(int n, vector<int> &dp) {
+        //top down hai
+        //base case
         if(n == 0) {
             return 0;
         }
         if(n == 1) {
             return 1;
         }
-        return fib(n-1)+fib(n-2);
+        if(dp[n] != -1) {
+            return dp[n];
+        }
+        dp[n] = solve(n-1, dp) + solve(n-2, dp);
+        return dp[n];
+    }
+    int fib(int n) {
+        // if(n == 0) {
+        //     return 0;
+        // }
+        // if(n == 1) {
+        //     return 1;
+        // }
+        // return fib(n-1)+fib(n-2);
+        vector<int> dp(n+1,-1);
+        return solve(n, dp);
         
     }
 };
