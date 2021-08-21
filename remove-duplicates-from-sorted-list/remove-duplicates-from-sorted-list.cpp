e65549b4a795a502195b0d4134e8b7ee70bf34f9
Keshav_Thakur <65155383+Keshav-Thakur8@users.noreply.github.com>
2021-08-21 13:21:15 +0530

    Update remove-duplicates-from-sorted-list.cpp

--- remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.cpp
+++ remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.cpp
@@ -11,17 +11,38 @@
 class Solution {
 public:
     ListNode* deleteDuplicates(ListNode* head) {
-        ListNode* temp = head;
-        while(temp != NULL && temp->next != NULL) {
-            if(temp->val == temp->next->val) {
-                temp->next = temp->next->next;
-                temp = temp->next;
-                temp = head;
+        //corner case
+        //thora fast hai
+        if(head == NULL || head->next == NULL) {
+            return head;
+        }
+        ListNode* prev = head;
+        ListNode* curr = head->next;
+        while(curr != NULL) {
+            if(prev->val == curr->val) {
+                prev->next = curr->next;
+                curr = curr->next;
             }
-            else {
-                temp = temp->next;
+            else{
+                prev = curr;
+                curr = curr->next;
             }
         }
+        
         return head;
+        
+        // ListNode* temp = head;
+        // while(temp != NULL && temp->next != NULL) {
+        //     if(temp->val == temp->next->val) {
+        //         temp->next = temp->next->next;
+        //         temp = temp->next;
+        //         //this will help in the case in which all elements of the list are same
+        //         temp = head;
+        //     }
+        //     else {
+        //         temp = temp->next;
+        //     }
+        // }
+        // return head;
     }
 };
