2ce4c7f8e7192a399efc43a5cf2a7165db750a3a
Keshav_Thakur <65155383+Keshav-Thakur8@users.noreply.github.com>
2021-09-14 19:07:22 +0530

    Update number-of-good-pairs.cpp

--- number-of-good-pairs/number-of-good-pairs.cpp
+++ number-of-good-pairs/number-of-good-pairs.cpp
@@ -3,12 +3,21 @@ public:
     int numIdenticalPairs(vector<int>& nums) {
         int ans = 0;
         int n = nums.size();
+        // for(int i=0 ; i<n ; i++) {
+        //     for(int j=i+1 ; j<n ; j++) {
+        //         if(nums[i] == nums[j]) {
+        //             ans++;
+        //         }
+        //     }
+        // }
+        // return ans;
+        
+        unordered_map<int, int> mp;
         for(int i=0 ; i<n ; i++) {
-            for(int j=i+1 ; j<n ; j++) {
-                if(nums[i] == nums[j]) {
-                    ans++;
-                }
-            }
+            mp[nums[i]]++;
+        }
+        for(auto it : mp) {
+            ans += (it.second * (it.second-1))/2;
         }
         return ans;
         
