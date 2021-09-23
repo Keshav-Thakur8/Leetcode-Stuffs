7f958920fc504e37277f0aa9dfb473ece93f795d
Keshav_Thakur <65155383+Keshav-Thakur8@users.noreply.github.com>
2021-09-23 10:07:37 +0530

    Time: 8 ms (76.48%), Space: 10.9 MB (99.06%) - LeetHub

--- single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
+++ single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -4,28 +4,43 @@ public:
         //binary search approach
         int n = nums.size();
         int si = 0;
-        int ei = n-1;
+        //int ei = n-1;
+        int ei = n-2;
         while(si <= ei){
-            int mid = si+(ei-si)/2;
+            //int mid = si+(ei-si)/2;
+            int mid = (si+ei)/2;
             //if mid is the answer
-            if((mid==0 || nums[mid] != nums[mid-1]) && (mid==n-1 || nums[mid] != nums[mid+1]))
-                return nums[mid];
+            
+            if(nums[mid] == nums[mid^1])
+                si=mid + 1;
+            else
+                ei = mid - 1;   
+            
+            
+            
+            
+            
+            
+            
+            
+            // if((mid==0 || nums[mid] != nums[mid-1]) && (mid==n-1 || nums[mid] != nums[mid+1]))
+            //     return nums[mid];
             
             //if mid is not the answer
-            if(nums[mid] == nums[mid-1]){
-                if(mid%2==1)
-                    si=mid+1;
-                else
-                    ei=mid-1;
-            }
-            else{
-                if(mid%2==1)
-                    ei=mid-1;
-                else
-                    si=mid+1;
-            }
+            // if(nums[mid] == nums[mid-1]){
+            //     if(mid%2==1)
+            //         si=mid+1;
+            //     else
+            //         ei=mid-1;
+            // }
+            // else{
+            //     if(mid%2==1)
+            //         ei=mid-1;
+            //     else
+            //         si=mid+1;
+            // }
         }
-        return -1;
+        return nums[si];
         
         // //xor approach
         // int n = nums.size();
