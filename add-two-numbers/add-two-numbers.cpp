3f5e35da6a2daa82f432ccccc816e78016e0074e
Keshav_Thakur <65155383+Keshav-Thakur8@users.noreply.github.com>
2021-10-02 10:54:46 +0530

    Time: 53 ms (19.85%), Space: 72.4 MB (10.80%) - LeetHub

--- add-two-numbers/add-two-numbers.cpp
+++ add-two-numbers/add-two-numbers.cpp
@@ -11,25 +11,40 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode* ans = new ListNode();
-        ListNode* temp = ans;
-        int carry = 0;
-        while(l1!=0 || l2!=0 || carry){
-            int sum = 0;
-            if(l1 != 0){
-                sum += l1->val;
-                l1 = l1->next;
-            }
-            if(l2 != 0){
-                sum += l2->val;
-                l2 = l2->next;
-            }
-            sum += carry;
-            carry = sum/10;
-            ListNode *node = new ListNode(sum%10);
-            temp->next = node;
-            temp = temp->next;
+        //recursive
+        if(l1 == NULL && l2 == NULL) {
+            return NULL;
         }
-        return ans->next;
+        if(l1 == NULL) return l2;
+        if(l2 == NULL) return l1;
+        
+        int x = l1->val + l2->val;
+        ListNode* node = new ListNode(x%10);
+        node->next = addTwoNumbers(l1->next, l2->next);
+        if(x >= 10) {
+            node->next = addTwoNumbers(node->next, new ListNode(1));
+        }
+        
+        return node;
+        // ListNode* ans = new ListNode();
+        // ListNode* temp = ans;
+        // int carry = 0;
+        // while(l1!=0 || l2!=0 || carry!=0){
+        //     int sum = 0;
+        //     if(l1 != 0){
+        //         sum += l1->val;
+        //         l1 = l1->next;
+        //     }
+        //     if(l2 != 0){
+        //         sum += l2->val;
+        //         l2 = l2->next;
+        //     }
+        //     sum += carry;
+        //     carry = sum/10;
+        //     ListNode *node = new ListNode(sum%10);
+        //     temp->next = node;
+        //     temp = temp->next;
+        // }
+        // return ans->next;
     }
 };
