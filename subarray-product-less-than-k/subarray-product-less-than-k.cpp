f2908c0426e7bf02772226a3442f59b959e886c3
Keshav_Thakur <65155383+Keshav-Thakur8@users.noreply.github.com>
2021-10-07 11:47:32 +0530

    Time: 72 ms (81.58%), Space: 61.3 MB (69.83%) - LeetHub

--- subarray-product-less-than-k/subarray-product-less-than-k.cpp
+++ subarray-product-less-than-k/subarray-product-less-than-k.cpp
@@ -1,26 +1,23 @@
 class Solution {
 public:
     int numSubarrayProductLessThanK(vector<int>& nums, int k) {
-        int cnt = 0;
-        int start = 0;
-        int end = 0;
-        int product = 1;
         int n = nums.size();
-        while(end < n){
-            product = product*nums[end];
-            //if the product is greater then k then divide the value at the start index from the given
-            //array and increase the start
-            while(start<n && product>=k){
-                product /= nums[start];
+        int start=0;
+        int end=0;
+        int result=1;
+        int count=0;
+        while(end<n){
+            result*=nums[end];
+            while(start<n and result>=k){
+                result/=nums[start];
                 start++;
             }
-            if(product < k){
-                //tricky step
-                cnt += end-start+1;
+            if(result<k){
+                count+=end-start+1;
             }
             end++;
         }
+        return count;
         
-        return cnt;
     }
 };
