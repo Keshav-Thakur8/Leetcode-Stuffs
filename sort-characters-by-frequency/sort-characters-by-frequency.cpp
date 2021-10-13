589dc06cbc79c0ffee0a7c2e2be606ca066c8e0a
Keshav_Thakur <65155383+Keshav-Thakur8@users.noreply.github.com>
2021-10-13 12:07:48 +0530

    Time: 273 ms (5.03%), Space: 376 MB (5.45%) - LeetHub

--- sort-characters-by-frequency/sort-characters-by-frequency.cpp
+++ sort-characters-by-frequency/sort-characters-by-frequency.cpp
@@ -1,18 +1,28 @@
+// vector<int> freq(256,0);
+// bool compare(char a, char b) {
+//     // < operator --> ascending order
+//     // > operator --> descending order
+//     if(freq[a] == freq[b]) {
+//         return a<b;
+//     }
+//     return freq[a]>freq[b];
+// }
+
 class Solution {
 public:
     string frequencySort(string s) {
         vector<int> freq(256,0);
-
-        for(char c:s) freq[c]++;
-
-        sort(s.begin(),s.end(),[=](char a,char b){
-
-            if(freq[a]==freq[b]) return a<b;
-
-            else return freq[a]>freq[b];
-
+        for(char ch : s) {
+            freq[ch]++;
+        }
+        // sort(s.begin(), s.end(), compare);
+        sort(s.begin(), s.end(), [=](char a, char b) {
+            if(freq[a] == freq[b]) {
+                return a<b;
+            }
+            return freq[a]>freq[b];
         });
-
+        
         return s;
     }
 };
