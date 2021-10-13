9bc123d8d1bfce260fcc951cb2e3d16c5432d44e
Keshav_Thakur <65155383+Keshav-Thakur8@users.noreply.github.com>
2021-10-13 12:54:05 +0530

    Time: 3 ms (43.08%), Space: 6.6 MB (12.22%) - LeetHub

--- custom-sort-string/custom-sort-string.cpp
+++ custom-sort-string/custom-sort-string.cpp
@@ -1,7 +1,9 @@
 class Solution {
 public:
     string customSortString(string S, string T) {
-        sort(T.begin(),T.end(),[=](char a,char b){return S.find(a)<S.find(b);});
+        sort(T.begin(),T.end(),[=](char a,char b) {
+            return S.find(a)<S.find(b);
+        });
         return T;
     }
 };
