cca998a359f8432e5e016237520264bdf2ea8a19
Keshav_Thakur <65155383+Keshav-Thakur8@users.noreply.github.com>
2022-02-16 10:59:40 +0530

    Time: 10 ms (6.98%), Space: 8.5 MB (65.07%) - LeetHub

--- 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.cpp
+++ 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.cpp
@@ -11,59 +11,59 @@
  */
 class Solution {
 public:
-    // //using 2 stack
-    // void helper(TreeNode* root, vector<int> &res) {
-    //     if(root == NULL) {
-    //         return;
-    //     }
-    //     stack<TreeNode*> st1, st2;
-    //     st1.push(root);
-    //     while(!st1.empty()) {
-    //         root = st1.top();
-    //         st1.pop();
-    //         st2.push(root);
-    //         if(root->left) {
-    //             st1.push(root->left);
-    //         }
-    //         if(root->right) {
-    //             st1.push(root->right);
-    //         }
-    //     }
-    //     while(!st2.empty()) {
-    //         res.push_back(st2.top()->val);
-    //         st2.pop();
-    //     }
-    // }
-    
-    //using single stack : ratna hai isko
+    //using 2 stack
     void helper(TreeNode* root, vector<int> &res) {
         if(root == NULL) {
             return;
         }
-        stack<TreeNode*> st;
-        TreeNode* current = root;  
-        while(current != NULL || !st.empty()) {
-            if(current != NULL){
-                st.push(current);
-                current = current->left;
-            }else{
-                TreeNode* temp = st.top()->right;
-                if (temp == NULL) {
-                    temp = st.top();
-                    st.pop(); 
-                    res.push_back(temp->val);
-                    while (!st.empty() && temp == st.top()->right) {
-                        temp = st.top();
-                        st.pop(); 
-                        res.push_back(temp->val);
-                    }
-                } 
-                else {
-                    current = temp;
-                }
+        stack<TreeNode*> st1, st2;
+        st1.push(root);
+        while(!st1.empty()) {
+            root = st1.top();
+            st1.pop();
+            st2.push(root);
+            if(root->left) {
+                st1.push(root->left);
             }
-        } 
+            if(root->right) {
+                st1.push(root->right);
+            }
+        }
+        while(!st2.empty()) {
+            res.push_back(st2.top()->val);
+            st2.pop();
+        }
     }
+    
+    // //using single stack : ratna hai isko
+    // void helper(TreeNode* root, vector<int> &res) {
+    //     if(root == NULL) {
+    //         return;
+    //     }
+    //     stack<TreeNode*> st;
+    //     TreeNode* current = root;  
+    //     while(current != NULL || !st.empty()) {
+    //         if(current != NULL){
+    //             st.push(current);
+    //             current = current->left;
+    //         }else{
+    //             TreeNode* temp = st.top()->right;
+    //             if (temp == NULL) {
+    //                 temp = st.top();
+    //                 st.pop(); 
+    //                 res.push_back(temp->val);
+    //                 while (!st.empty() && temp == st.top()->right) {
+    //                     temp = st.top();
+    //                     st.pop(); 
+    //                     res.push_back(temp->val);
+    //                 }
+    //             } 
+    //             else {
+    //                 current = temp;
+    //             }
+    //         }
+    //     } 
+    // }
     
     // //recursive
     // void helper(TreeNode* root, vector<int> &res) {
