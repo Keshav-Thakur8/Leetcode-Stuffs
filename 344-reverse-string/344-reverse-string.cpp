8e32cb91adad89bb7032142f64c212cde451e9f4
Keshav_Thakur <65155383+Keshav-Thakur8@users.noreply.github.com>
2022-07-29 14:29:15 +0530

    Time: 36 ms (45.27%), Space: 23.2 MB (40.38%) - LeetHub

--- 344-reverse-string/344-reverse-string.cpp
+++ 344-reverse-string/344-reverse-string.cpp
@@ -1,14 +1,22 @@
 class Solution {
 public:
-    void helper(int left_idx, int right_idx, vector<char>& s) {
-        if(left_idx >= right_idx) {
+    void helper(int left_idx, vector<char>& s, int n) {
+        if(left_idx >= n/2) {
             return;
         }
-        swap(s[left_idx], s[right_idx]);
-        helper(left_idx+1, right_idx-1, s);
+        swap(s[left_idx], s[n-left_idx-1]);
+        helper(left_idx+1, s, n);
     }
+    // void helper(int left_idx, int right_idx, vector<char>& s) {
+    //     if(left_idx >= right_idx) {
+    //         return;
+    //     }
+    //     swap(s[left_idx], s[right_idx]);
+    //     helper(left_idx+1, right_idx-1, s);
+    // }
     void reverseString(vector<char>& s) {
         int n = s.size();
-        helper(0, n-1, s);
+        //helper(0, n-1, s);
+        helper(0, s, n);
     }
 };
